0a2a2a4b4efa10f1043d894f73f086e93f7bbdd0
Scott Bowler <scott.bowler@learningfuze.com>
2018-10-17 00:45:07 -0700

    Initial commit

---
+++ Models/MvcMovieContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options) : base(options) {}
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
